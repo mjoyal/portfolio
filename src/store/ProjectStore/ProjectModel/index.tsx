95b71e5c0e717e7d901cc092c677ea07264093a0
mjoyal <mackenzie.joyal@gmail.com>
2025-05-06 15:02:24 -0700

    add quick description

--- src/store/ProjectStore/ProjectModel/index.tsx
+++ src/store/ProjectStore/ProjectModel/index.tsx
@@ -2,14 +2,30 @@ import { Instance, types } from "mobx-state-tree";
 
 export type IProjectModel = Instance<typeof ProjectModel>;
 
-const ProjectModel = types.model("ProjectModel", {
-  id: types.identifierNumber,
-  name: types.maybe(types.string),
-  slug: types.string,
-  role: types.maybe(types.string),
-  imageSource: types.string,
-  heroImageSource: types.maybe(types.string),
-  description: types.maybe(types.string),
-});
+const ProjectModel = types
+  .model("ProjectModel", {
+    id: types.identifierNumber,
+    name: types.maybe(types.string),
+    slug: types.string,
+    role: types.maybe(types.string),
+    imageSource: types.string,
+    heroImageSource: types.maybe(types.string),
+    description: types.maybe(types.string),
+  })
+  .views((self) => {
+    return {
+      get quickDescription(): string {
+        if (self.slug === "fit52") {
+          return "Carrie Underwood's fitness app";
+        }
+
+        if (self.slug === "crewlywed") {
+          return "Socket.io multiplayer web game";
+        }
+
+        return "Discovering live local music gigs";
+      },
+    };
+  });
 
 export default ProjectModel;
