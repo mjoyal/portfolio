59df4cb1a893d7a6d2f1d3d325c3e3bac34a1f2b
mjoyal <mackenzie.joyal@gmail.com>
2025-05-13 16:07:46 -0700

    add components folder for projects

---
+++ src/pages/Project/components/index.tsx
@@ -0,0 +1,44 @@
+import styled from "@emotion/styled";
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+import Spacer from "src/components/Spacer";
+
+export const StyledP = styled.p<{ isHidden?: boolean }>`
+  max-width: 400px;
+  display: ${({ isHidden = false }) => (isHidden ? "none" : "block")};
+`;
+
+export const ProjectImage = styled.img`
+  width: 100%;
+  height: auto;
+  display: block;
+  object-fit: cover;
+  border-radius: 3px;
+`;
+
+export const ProjectPageContainer = styled(Box)`
+  /* These 100% widths are important to keep the overall margin of the site */
+  width: 100%;
+  min-height: 100vh;
+  margin-top: 40px;
+  ${({ theme }) => (theme as any).breakpoints.up("md")} {
+    padding-left: 60px;
+    padding-right: 60px;
+  }
+`;
+
+export const PhoneMockImage = styled.img`
+  width: 300px;
+  height: 100%;
+`;
+
+export const StyledTitle = styled.h4`
+  max-width: 700px;
+`;
+
+export const SectionDivider = () => {
+  const theme = useTheme();
+
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
+
+  return isSmallScreen ? <Spacer level={8} /> : <Spacer level={30} />;
+};
